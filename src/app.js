31a5ec69386283d07be495a07b1db49e0015f9ac
dkraemerwork <dkraemerwork@gmail.com>
2019-11-15 15:32:56 +0100

    Adds express functionality

--- src/app.js
+++ src/app.js
@@ -1,5 +1,15 @@
-const http = require('http');
-const handler = require('./routes');
+const  express = require("express");
 
-const server = http.createServer(handler);
-server.listen(1337);
+const app = express();
+
+app.use(("/add-product"),(req, res, next) => {
+    console.log("In another middleware");
+    res.send('<h1>Add Product</h1>');
+});
+
+app.use(("/"),(req, res, next) => {
+    console.log("In another middleware");
+    res.send('<h1>hello From express</h1>');
+});
+
+app.listen(1337);
